7f5a1cef7a695a4169d414a1828ed4b452c3009b
daichi0918 <kayasugadaichi@icloud.com>
2026-01-23 11:24:25 +0900

    fin:125

--- main.go
+++ main.go
@@ -23,17 +23,17 @@ func main() {
 
 	// u.CreateUser()
 
-	u, _ := models.GetUser(1)
+	// u, _ := models.GetUser(1)
 
-	fmt.Println(u)
+	// fmt.Println(u)
 
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 }
