8c27c1a5ba3d506f175c35a8738441b44a0e2b1e
daichi0918 <kayasugadaichi@icloud.com>
2026-01-23 11:37:07 +0900

    fin:127

--- main.go
+++ main.go
@@ -39,6 +39,15 @@ func main() {
 
 		// user, _ := models.GetUser(2)
 	// user.CreateTodo("First todo")
-	t, _ := models.GetTodo(1)
-	fmt.Println(t)
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+
+	user, _ := models.GetUser(2)
+	user.CreateTodo("Second todo")
+
+	todos, _ := models.GetTodos()
+	for _, v := range todos {
+		fmt.Println(v)
+	}
+	// fmt.Println(todos)
 }
