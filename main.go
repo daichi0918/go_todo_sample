b7d01b2b7cf109bbd0a96322bd29aee2bb20f64d
daichi0918 <kayasugadaichi@icloud.com>
2026-01-24 10:06:24 +0900

    fin:133

--- main.go
+++ main.go
@@ -2,63 +2,15 @@ package main
 
 import (
 	"fmt"
+	"go_sample_todo/app/controllers"
 	"go_sample_todo/app/models"
 )
 
 func main() {
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.SQLDriver)
-	// fmt.Println(config.Config.DbName)
-	// fmt.Println(config.Config.LogFile)
-
-	// log.Println("test")
-
-	// fmt.Println(models.Db)
-
-	u := &models.User{}
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.PassWord = "password"
-	fmt.Println(u);
-
-	u.CreateUser()
 
 
+	fmt.Println(models.Db)
 
-	// fmt.Println(u)
+	controllers.StartMainServer()
 
-	// u.Name = "Test2"
-	// u.Email = "test2@example.com"
-	// u.UpdateUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.DeleteUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-		// user, _ := models.GetUser(2)
-	// user.CreateTodo("First todo")
-	// t, _ := models.GetTodo(1)
-	// fmt.Println(t)
-
-	// user, _ := models.GetUser(3)
-	// user.CreateTodo("Third todo")
-
-	// todos, _ := models.GetTodos()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-	// fmt.Println(todos)
-
-	// user2, _ := models.GetUser(3)
-	// todos, _ := user2.GetTodosByUser()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-		t, _ := models.GetTodo(3)
-		t.DeleteTodo()
-	// 	t.Content = "Updated todo content"
-	// fmt.Println(t)
 }
