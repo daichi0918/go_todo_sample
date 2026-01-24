b52afc62d6bee8ff3ded4874ccd463802b87c27f
daichi0918 <kayasugadaichi@icloud.com>
2026-01-24 10:15:51 +0900

    fin:135

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -47,8 +47,8 @@ func parseURL(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc
 }
 
 func StartMainServer() error {
-	// files := http.FileServer(http.Dir(config.Config.Static))
-	// http.Handle("/static/", http.StripPrefix("/static/", files))
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
 
 	http.HandleFunc("/", top) //top
 	// http.HandleFunc("/signup", signup)
